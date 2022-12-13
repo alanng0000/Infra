e2d0a3f735530d302630ba61fff1238e5be4458f
Alan <terraplatform10@gmail.com>
2022-12-13 17:54:18 +0800

    Exe

--- Infra.Exe/Main.c
+++ Infra.Exe/Main.c
@@ -53,9 +53,264 @@ Bool ConsoleWrite(Object string)
 
 
 
+Bool ControlHandleConsoleWriteKeyValue(Int controlKey, Int controlValue)
+{
+    Object format;
+
+
+
+    format = Format_New();
+
+
+
+    Format_Init(format);
+
+
+
+
+
+    Int o = CastInt(", \n");
+
+
+
+    Int baseLength = String_ConstantLength(o);
+
+
+
+
+    Object base;
+    
+
+
+    base = String_New();
+
+
+
+    String_Init(base);
+
+
+
+
+    String_SetLength(base, baseLength);
+
+
+    String_SetData(base, o);
+
+
+
+
+
+
+
+    Object result = String_New();
+
+
+
+    String_Init(result);
+
+
+
+
+
+    Int arg = 0;
+
+
+    Int index = 0;
+
+
+    Int type = 0;
+
+
+    Int value = 0;
+
+
+
+    Int argCount = 2;
+
+
+
+
+    Format_SetBase(format, base);
+
+
+
+
+    Format_SetArgCount(format, argCount);
+
+
+
+
+    Format_Open(format);
+
+
+
+
+
+
+
+    arg = 0;
+
+
+    index = 0;
+
+
+    type = 1;
+
+
+    value = controlKey;
+
+
+
+
+    SetFormatArg(format, arg, index, type, value);
+
+
+
+
+
+
+    arg = 1;
+
+
+    index = 2;
+
+
+    type = 1;
+
+    
+    value = controlValue;
+
+
+
+
+    SetFormatArg(format, arg, index, type, value);
+
+
+
+
+
+
+
+    Int resultLength = Format_Length(format);
+
+
+
+
+    Int resultBuffer = New(resultLength);
+
+
+
+
+    String_SetLength(result, resultLength);
+
+
+
+    String_SetData(result, resultBuffer);
+
+
+
+
+    Format_SetString(format, result);
+
+
+
+
+
+
+    Format_Result(format);
+
+
+
+
+
+    Format_Close(format);
+
+
+
+
+
+
+
+
+    Object console;
+    
+    
+
+    console = Console_New();
+
+
+
+    Console_Init(console);
+
+
+
+    Console_Write(console, result);
+
+
+
+    Console_Final(console);
+
+
+
+    Console_Delete(console);
+
+
+
+
+
+
+
+
+    Delete(resultBuffer);
+
+
+
+
+
+    String_Final(result);
+
+
+
+    String_Delete(result);
+
+
+
+
+
+
+    String_Final(base);
+
+
+
+    String_Delete(base);
+
+
+
+
+
+    Format_Final(format);
+
+
+
+    Format_Delete(format);
+
+
+
+
+    return true;
+}
+
+
+
+
 
 Bool ControlHandle(Object frame, Int key, Int value)
 {
+    ControlHandleConsoleWriteKeyValue(key, value);
+
+
+
+
     if (key == 0x20)
     {
         if (value == 1)
