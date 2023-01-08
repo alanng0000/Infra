db613c547c938e739d31448099d123b7b79fd98b
Alan <terraplatform10@gmail.com>
2023-01-08 22:38:32 +0800

    Form

--- Infra.Form.Windows/Font.c
+++ Infra.Form.Windows/Font.c
@@ -35,7 +35,7 @@ Int Font_Create(Int nameLength, Int name, Int size, Int weight, Bool italic, Boo
     
     
     u = Windows_CreateFontA(size, 0, 0, 0, weight, ui, uu, 
-        FALSE, ANSI_CHARSET, DWORD iOutPrecision, DWORD iClipPrecision, 
+        FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, 
         DWORD iQuality, DWORD iPitchAndFamily, LPCSTR pszFaceName);
 
 
